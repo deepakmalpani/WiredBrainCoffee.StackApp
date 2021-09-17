b84019f9f7175c9fbb459892520e1f6fb159d6c1
Deepak Malpani <deepak.malpani@exxonmobil.com>
2021-09-17 12:12:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WiredBrainCoffee.StackApp
+ Application to load and store employee and organisation data
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WiredBrainCoffee.StackApp
+{
+    internal class SimpleStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+
+        public int Count => _currentIndex + 1;
+
+        public SimpleStack() => _items = new double[10];
+
+
+        public void Push(double item) => _items[++_currentIndex] = item;
+
+        public double Pop() => _items[_currentIndex--];
+        
+    }
+}
