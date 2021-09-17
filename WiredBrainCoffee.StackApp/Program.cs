3a896a6853a710d15912da20b3301b81867ca5a9
Deepak Malpani <deepak.malpani@exxonmobil.com>
2021-09-17 12:21:27 +0530

    Implementing Stack string method using object class
    Implementing Stack string method using object class

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -6,8 +6,38 @@ namespace WiredBrainCoffee.StackApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            StackDoubles();
+            StackStrings();
+            Console.ReadLine();
         }
 
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(3.2);
+            stack.Push(4.5);
+            stack.Push(9.1);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = (double)stack.Pop();
+                Console.WriteLine($"Item : {item}");
+                sum += item;
+            }
+            Console.WriteLine($"Sum : {sum}");
+        }
+        private static void StackStrings()
+        {
+            var stack = new SimpleStack();
+            stack.Push("Weird Brain Coffee");
+            stack.Push("Pluralsight");
+            while (stack.Count > 0)
+            {
+                Console.WriteLine($"Item : {stack.Pop()}");
+            }
+        }
     }
 }
